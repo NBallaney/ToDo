7a950e26e01fed88ceca45c39bfdc2d3f4335474
Nikhil Ballaney <nballaney@gmail.com>
2015-04-13 12:28:23 -0700

    [tests] Tests create and retreive

--- server_tests.py
+++ server_tests.py
@@ -23,20 +23,53 @@ class serverTestCase(unittest.TestCase):
 
 
   # Function name should start with 'test' so that unittest runs it automatically.
+  def test_config_settings(self):
+    config = server.app.config
+    assert os.path.exists("todos.db")
+    assert config['TESTING']
+
+
   def test_empty_db(self):
     response = self.app.get('/api')
     assert '[]' in response.data
 
 
-  def test_create(self):
-    todo = {"text": "Testing 1",
-            "id": 1}
+  # Helper function to create todos.
+  def create_todo(self, id, text):
+    todo = {"text": text,
+            "id": id}
 
     response = self.app.post('/api',
                               data=json.dumps(todo),
                               content_type='application/json')
+    return response
 
+
+  # Helper function to retrieve todos
+  def retrieve_todos(self):
+    response = self.app.get('/api')
+    json_response = json.loads(response.data)
+    return json_response
+
+
+
+
+  def test_message_create(self):
+    response = self.create_todo(1, "Test 1")
     assert '[]' not in response.data
+
+
+  def test_messages_retreive(self):
+    self.create_todo(1, 'Test 1')
+    self.create_todo(2, 'Test 2')
+    response = self.retrieve_todos()
+
+    assert response[0]['id'] == 1
+    assert response[0]['text'] == 'Test 1'
+    assert response[1]['id'] == 2
+    assert response[1]['text'] == 'Test 2'
+
+
 
 
 if __name__ == '__main__':
