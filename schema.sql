c630139d26ce5580dba978f157913d95a02ddc81
Nikhil Ballaney <nballaney@gmail.com>
2015-04-12 21:44:43 -0700

    [feat] Initialize database through schema.sql

---
+++ schema.sql
@@ -0,0 +1,5 @@
+drop table if exists todos;
+create table todos (
+  id integer,
+  text text not null
+);
--- server.py
+++ server.py
@@ -1,16 +1,34 @@
 import sqlite3
 import os
 from flask import Flask, g, request, render_template, json
+from contextlib import closing
+
 
 app = Flask(__name__, static_url_path='', static_folder='')
+app.config['DATABASE'] = os.path.join(app.root_path, 'todos.db')
 
 
-@app.before_request
-def before_request():
-  g.db = sqlite3.connect("todos.db")
+def connect_db():
+  rv = sqlite3.connect(app.config['DATABASE'])
+  rv.row_factory = sqlite3.Row
+  return rv
 
 
-# Teardown request is always executed, even if before_request fails.
+def get_db():
+  if not hasattr(g, 'db'):
+      g.db = connect_db()
+  return g.db
+
+
+def init_db():
+  db = get_db()
+  with closing(connect_db()) as db:
+    with app.open_resource('schema.sql', mode='r') as f:
+        db.cursor().executescript(f.read())
+    db.commit()
+
+
+# Teardown request is always executed after every request
 @app.teardown_request
 def teardown_request(exception):
   if hasattr(g, 'db'):
@@ -25,7 +43,8 @@ def returnIndexPage():
 @app.route('/api')
 def get_todos():
   # Returns a list of tuples.
-  data = g.db.execute("SELECT text,id FROM todos").fetchall()
+  db = get_db()
+  data = db.execute("SELECT text,id FROM todos").fetchall()
   todos = []
   for i in data:
     todos.append({'text': i[0], 'id': i[1]})
@@ -37,23 +56,26 @@ def post_todo():
   jsonData = request.get_json()
   text = jsonData['text']
   id = jsonData['id']
-  g.db.execute("INSERT INTO todos (id, text) VALUES (?, ?)", [id, text])
-  g.db.commit()
+  db = get_db()
+  db.execute("INSERT INTO todos (id, text) VALUES (?, ?)", [id, text])
+  db.commit()
   return 'ToDo Created'
 
 
 @app.route('/api/<id>', methods = ['DELETE'])
 def delete_todo(id):
-  g.db.execute("DELETE FROM todos WHERE id=(?)", [id])
-  g.db.commit()
+  db = get_db()
+  db.execute("DELETE FROM todos WHERE id=(?)", [id])
+  db.commit()
   return 'ToDo Completed'
 
 
 @app.route('/api/<id>', methods = ['PUT'])
 def update_todo(id):
   text = request.get_json()['text']
-  g.db.execute("UPDATE todos SET text=(?) WHERE id=(?)", [text, id])
-  g.db.commit()
+  db = get_db()
+  db.execute("UPDATE todos SET text=(?) WHERE id=(?)", [text, id])
+  db.commit()
   return 'ToDo Updated'
 
 
